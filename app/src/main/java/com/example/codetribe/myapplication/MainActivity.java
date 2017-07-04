a1c976914a3bacb5bdc92edf4f687be769776518
mollymolz <mollysaadiq@gmail.com>
2017-07-04 10:07:38 +0200

    Initial commit

---
+++ app/src/main/java/com/example/codetribe/myapplication/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.codetribe.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "molzMessage";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart");
+    }
+}
